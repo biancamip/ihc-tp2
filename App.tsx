423ad509164049ed6e68c6bbc9c915361cf89f24
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-07-17 16:09:49 -0300

    remove unused import

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
