a00e956801842c41f356eb0dcc0670bd76ac5745
Bianca Pelegrini <bmpelegrini@gmail.com>
2022-07-17 15:50:55 -0300

    ex 1.1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+type Inputs = { value1: string | null; value2: string | null };
 
 const App = () => {
+  const [inputs, setInputs] = useState<Inputs>({ value1: null, value2: null });
+  const [result, setResult] = useState<number | null>(null);
+
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>Atividade 1</Text>
+          <Text style={styles.sectionTitle}>Atividade 1.1</Text>
+          <TextInput
+            style={styles.textInput}
+            keyboardType="numeric"
+            placeholder="Insira o primeiro número"
+            value={inputs.value1 ?? ''}
+            onChangeText={ev => setInputs({ ...inputs, value1: ev })}
+          />
+          <TextInput
+            style={styles.textInput}
+            keyboardType="numeric"
+            placeholder="Insira o segundo número"
+            value={inputs.value2 ?? ''}
+            onChangeText={ev => setInputs({ ...inputs, value2: ev })}
+          />
+          <Button
+            title="Somar"
+            disabled={
+              inputs.value1 === null ||
+              inputs.value1 === '' ||
+              inputs.value2 === null ||
+              inputs.value2 === ''
+            }
+            onPress={() => {
+              inputs.value1 !== null &&
+                inputs.value2 !== null &&
+                setResult(+inputs.value1 + +inputs.value2);
+            }}
+          />
+
+          {result !== null && (
+            <>
+              <Text style={{ ...styles.sectionTitle, marginTop: 8 }}>
+                Resultado
+              </Text>
+              <Text>{result}</Text>
+            </>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
